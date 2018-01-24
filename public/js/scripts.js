282c31e055ac336c3225a24597ed3fd1850b6da0
Oleg Dubrovsky <olegdubrowsky@gmail.com>
2018-01-24 11:28:46 +0200

    second

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -5,21 +5,18 @@ var params = "/collaboration";
 var id;
 var status;
 
-setStars();
+table.addEventListener("click", setParams, false);
+setInterval(setStars, 1000);
 
 
-table.addEventListener("click", myFunction, false);
-
-function myFunction(event) {
+function setParams(event) {
   if(event.target.tagName !== 'IMG') return;
     for(var i = 0; i<100; i++){
       if(event.target === stars[i]){
       id = i;
-      el = event.target;
       }
     }
-     if(el.getAttribute('SRC') === "star_off.gif"){
-     	console.log(event.target.getAttribute('scr'));
+     if(event.target.getAttribute('SRC') === "star_off.gif"){
       status = 1;
      }
       else {
@@ -32,16 +29,13 @@ function myFunction(event) {
 function setStars(){
   var xhr = new XMLHttpRequest();
   xhr.open('GET', params, true);
-  console.log(params);
   xhr.send();
-  console.log(xhr);
   xhr.onreadystatechange = function() {
       if (this.readyState != 4) return;
     if (this.status != 200) {
       alert( 'ошибка: ' + (this.status ? this.statusText : 'запрос не удался'));
       return;
     }
-    console.log(xhr.responseText[id]);
     for(var i = 0; i<100; i++){
       if(xhr.response[i] == 0){
         stars[i].setAttribute ("src", "star_off.gif");
@@ -53,6 +47,5 @@ function setStars(){
   }
 }
 
-setInterval(setStars, 1000);
 
 
