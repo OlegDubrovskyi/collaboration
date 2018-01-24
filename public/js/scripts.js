abda4675b12627325815249a6b1dfa9e59277a9a
OlegItCloud <olegdubrowsky@gmail.com>
2018-01-24 09:50:25 +0200

    finish

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,12 +1,16 @@
 var table = document.getElementById('star_table');
 var stars = table.getElementsByTagName('img');
 var url = '/collaboration';
-var params;
+var params = "/collaboration";
 var id;
 var status;
 
-table.onclick = function(event) {
-	
+setStars();
+
+
+table.addEventListener("click", myFunction, false);
+
+function myFunction(event) {
   if(event.target.tagName !== 'IMG') return;
     for(var i = 0; i<100; i++){
       if(event.target === stars[i]){
@@ -18,29 +22,37 @@ table.onclick = function(event) {
      	console.log(event.target.getAttribute('scr'));
       status = 1;
      }
-       else {
-         status = 0;
-       }
+      else {
+        status = 0;
+      }
    params = url+'?n='+id+'&s='+status;
-   var xhr = new XMLHttpRequest();
-	xhr.open('GET', params, true);
-	console.log(params);
-	xhr.send();
-	console.log(xhr);
-	xhr.onreadystatechange = function() {
-  	  if (this.readyState != 4) return;
-	  if (this.status != 200) {
-	    alert( 'ошибка: ' + (this.status ? this.statusText : 'запрос не удался'));
-	    return;
-	  }
-	  console.log(xhr.responseText[id]);
-	  if(xhr.responseText[id] == 0){
-	  	el.setAttribute ("src", "star_on.gif");
-	  }
-	  if(xhr.responseText[id] == 1){
-	  	el.setAttribute ("src", "star_off.gif");
-	  }
-  }
-  
+   setStars();
 }
+
+function setStars(){
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', params, true);
+  console.log(params);
+  xhr.send();
+  console.log(xhr);
+  xhr.onreadystatechange = function() {
+      if (this.readyState != 4) return;
+    if (this.status != 200) {
+      alert( 'ошибка: ' + (this.status ? this.statusText : 'запрос не удался'));
+      return;
+    }
+    console.log(xhr.responseText[id]);
+    for(var i = 0; i<100; i++){
+      if(xhr.response[i] == 0){
+        stars[i].setAttribute ("src", "star_off.gif");
+      }
+      if(xhr.response[i] == 1){
+        stars[i].setAttribute ("src", "star_on.gif");
+      }
+    }
+  }
+}
+
+setInterval(setStars, 1000);
+
 
